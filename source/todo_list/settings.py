253b95eb8f928c0ecd612adf9220dafdb7c740b1
anton_ign <stimovskijanton@gmail.com>
2026-06-04 02:38:13 +0600

    Логика дебага и 404 ошибок изменена

--- source/todo_list/settings.py
+++ source/todo_list/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p=_c030u70$k04ogpwx)$)^ckq-5upd6efz_^(cz)#eaup5e3c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
