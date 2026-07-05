6bc8ae91dd19c36a4a924e6b0d1c39bd84b97573
anton_ign <stimovskijanton@gmail.com>
2026-07-05 17:34:10 +0600

    начат третий этап, добавлена связь многие-ко-мнгогим в таблице Задач

--- source/todo_app/models/task.py
+++ source/todo_app/models/task.py
@@ -5,7 +5,7 @@ class TodoItem(models.Model):
     title = models.TextField(max_length=200, null=False, blank=False, verbose_name="Заголовок")
     description = models.TextField(max_length=500, null=True, blank=True, verbose_name="Описание")
     status = models.ForeignKey('todo_app.Status', related_name="task", on_delete=models.RESTRICT, null=False, blank=False, verbose_name="Статус")
-    type = models.ForeignKey('todo_app.Type', related_name="task", on_delete=models.RESTRICT, null=False, blank=False, verbose_name="Тип")
+    type = models.ManyToManyField('todo_app.Type', related_name="tasks", blank=True, verbose_name="Типы")
     date_of_add = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     date_of_update = models.DateField(auto_now=True, verbose_name="Последние изменения")
 
