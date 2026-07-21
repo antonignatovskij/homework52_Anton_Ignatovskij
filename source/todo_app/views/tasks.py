8a35144cd1f60888bb7f91975679e7ff4a9e0545
anton_ign <stimovskijanton@gmail.com>
2026-07-22 04:32:38 +0600

    защищены все страницы от неавторизованых пользователей, добавлены стили к форму входа

--- source/todo_app/views/tasks.py
+++ source/todo_app/views/tasks.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlencode
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
@@ -14,7 +15,7 @@ from todo_app.models import TodoItem, Project
 
 # Create your views here.
 
-class ProjectListView(ListView):
+class ProjectListView(LoginRequiredMixin, ListView):
     template_name = 'tasks/projects_list.html'
     model = Project
     context_object_name = 'projects'
@@ -25,6 +26,7 @@ class ProjectListView(ListView):
         self.form = self.get_search_form()
         self.search_value = self.get_search_value()
         return super().dispatch(request, *args, **kwargs)
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -47,42 +49,43 @@ class ProjectListView(ListView):
             return self.form.cleaned_data['search']
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(LoginRequiredMixin,DetailView):
     template_name = 'tasks/project_detail.html'
     model = Project
 
 
-class ProjectCreateView(CreateView):
+class ProjectCreateView(LoginRequiredMixin, CreateView):
     template_name = 'tasks/project_create.html'
     model = Project
     form_class = ProjectForm
 
     def get_success_url(self):
-        return reverse('project_detail', kwargs={'pk': self.object.pk})
+        return reverse('tasks:project_detail', kwargs={'pk': self.object.pk})
 
 
-class ProjectUpdateView(UpdateView):
+class ProjectUpdateView(LoginRequiredMixin,UpdateView):
     model = Project
     template_name = 'tasks/project_update.html'
     form_class = ProjectForm
     context_object_name = 'project'
 
+
     def get_success_url(self):
-        return reverse('project_detail', kwargs={'pk': self.object.pk})
+        return reverse('tasks:project_detail', kwargs={'pk': self.object.pk})
 
 
-class ProjectDeleteView(DeleteView):
+class ProjectDeleteView(LoginRequiredMixin,DeleteView):
     template_name = 'tasks/project_detail.html'
     model = Project
     context_object_name = 'project'
-    success_url = reverse_lazy('projects')
+    success_url = reverse_lazy('tasks:projects')
 
 
 
 # ////вьюшки для тасков ниже
 
 
-class TaskListView(ListView):
+class TaskListView(LoginRequiredMixin,ListView):
     template_name = 'tasks/index.html'
     context_object_name = 'tasks'
 
@@ -91,13 +94,15 @@ class TaskListView(ListView):
         return TodoItem.objects.all()
 
 
-class TaskDetailView(DetailView):
+
+class TaskDetailView(LoginRequiredMixin,DetailView):
     template_name = 'tasks/task_detail.html'
     model = TodoItem
     context_object_name = 'task'
 
 
-class TaskCreateView(CreateView):
+
+class TaskCreateView(LoginRequiredMixin,CreateView):
     model = TodoItem
     template_name = 'tasks/create_task.html'
     form_class = TaskForm
@@ -108,21 +113,25 @@ class TaskCreateView(CreateView):
         task.project = project
         task.save()
         task.type.set(form.cleaned_data['type'])
-        return redirect('project_detail', pk=task.project.pk)
+        return redirect('tasks:project_detail', pk=task.project.pk)
 
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(LoginRequiredMixin,UpdateView):
     model = TodoItem
     template_name = 'tasks/update_task.html'
     form_class = TaskForm
     context_object_name = 'task'
 
+
     def get_success_url(self):
-        return reverse('project_detail', kwargs={'pk': self.object.project.pk})
+        return reverse('tasks:project_detail', kwargs={'pk': self.object.project.pk})
 
 
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(LoginRequiredMixin,DeleteView):
     template_name = 'tasks/task_detail.html'
     model = TodoItem
     context_object_name = 'task'
-    success_url = reverse_lazy('projects')
+    success_url = reverse_lazy('tasks:projects')
+
+
+
--- source/todo_list/settings.py
+++ source/todo_list/settings.py
@@ -115,4 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'tasks:projects'
+LOGOUT_REDIRECT_URL = 'tasks:projects'
