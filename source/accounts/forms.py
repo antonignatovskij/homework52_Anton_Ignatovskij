c19929f5722d48d84594edaa7913461a73e0c469
anton_ign <stimovskijanton@gmail.com>
2026-07-24 03:10:26 +0600

    сделана регистрация

---
+++ source/accounts/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
